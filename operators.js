62b463ea96242903836cfad6b5a370fea07b4da4
kallwin675-wq <kallwin675@gmail.com>
2026-06-12 11:38:39 +0530

    sample

--- operators.js
+++ operators.js
@@ -1,5 +1,5 @@
 //arithmetic operators
-console.log("Arithmetic Operators:")
+/*console.log("Arithmetic Operators:")
 let a = 10;
 let b = 5;
 console.log(a + b); // Addition
@@ -19,4 +19,18 @@ console.log(c);
 c *= 5; // c = c * 5
 console.log(c);
 c /= 5; // c = c / 5
-console.log(c);
+console.log(c);*/
+
+//comparison operators
+console.log("Comparison Operators:")
+let x = 10;
+let y = 5;
+console.log(x == y); // Equal to
+console.log(x != y); // Not equal to
+console.log(x > y); // Greater than
+console.log(x < y); // Less than
+console.log(x >= y); // Greater than or equal to
+console.log(x <= y); // Less than or equal to 
+console.log(x === y); // Strict equal to
+console.log(x !== y); // Strict not equal to
+      
