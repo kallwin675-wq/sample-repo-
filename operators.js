b7569737dd8658265de8d69b320b0b089d5cca7b
kallwin675-wq <kallwin675@gmail.com>
2026-06-12 11:18:48 +0530

    sample

---
+++ operators.js
@@ -0,0 +1,22 @@
+//arithmetic operators
+console.log("Arithmetic Operators:")
+let a = 10;
+let b = 5;
+console.log(a + b); // Addition
+console.log(a - b); // Subtraction
+console.log(a * b); // Multiplication
+console.log(a / b); // Division
+console.log(a % b); // Modulus
+console.log(a ** b); // Exponentiation
+
+//assignment operators
+console.log("Assignment Operators:")
+let c = 10;
+c += 5; // c = c + 5
+console.log(c);
+c -= 5; // c = c - 5
+console.log(c);
+c *= 5; // c = c * 5
+console.log(c);
+c /= 5; // c = c / 5
+console.log(c);
